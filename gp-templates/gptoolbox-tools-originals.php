a5e7d8f3713495bfab4eb6eccc46362ae1ec92ab
pedro-mendonca <ped.gaspar@gmail.com>
2024-01-23 15:27:35 +0000

    Update readme

--- gp-templates/gptoolbox-tools-originals.php
+++ gp-templates/gptoolbox-tools-originals.php
@@ -15,8 +15,8 @@ namespace GP_Toolbox;
 </p>
 <h4 class="features"><?php esc_html_e( 'Features', 'gp-toolbox' ); ?></h4>
 <ul>
-	<li><?php esc_html_e( 'Find and delete Originals for unknown Projects. (e.g. Related Project was deleted)', 'gp-toolbox' ); ?></li>
-	<li><?php esc_html_e( 'Find and delete obsolete Originals.', 'gp-toolbox' ); ?></li>
+	<li><?php esc_html_e( 'Find Originals for unknown Projects. (e.g. Related Project was deleted)', 'gp-toolbox' ); ?></li>
+	<li><?php esc_html_e( 'Find obsolete Originals.', 'gp-toolbox' ); ?></li>
 </ul>
 
 <?php
