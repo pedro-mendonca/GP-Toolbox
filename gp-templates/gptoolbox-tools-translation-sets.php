24fd3c77afb8d906ff7975088f8c7e3ba56e089b
pedro-mendonca <ped.gaspar@gmail.com>
2024-01-30 11:26:45 +0000

    Add missing Translation Sets tools description

---
+++ gp-templates/gptoolbox-tools-translation-sets.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template file.
+ *
+ * @package GP_Toolbox
+ *
+ * @since 1.0.0
+ */
+
+namespace GP_Toolbox;
+
+?>
+<p class="descrtiption">
+	<?php esc_html_e( 'Overview of all Translation Sets.', 'gp-toolbox' ); ?>
+</p>
+<ul>
+	<li><?php esc_html_e( 'Find Translation Sets of unknown Projects. (e.g. Related Project was deleted)', 'gp-toolbox' ); ?></li>
+</ul>
+
+<?php
+// TODO: One click clean Translation Set for unknown projects.
